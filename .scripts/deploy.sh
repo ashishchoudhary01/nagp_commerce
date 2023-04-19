d5d860c4f8d051b4786f2cd11dde754696bd06b2
Ashish Choudhary <ashish.choudhary01@nagarro.com>
2023-04-19 07:34:18 +0530

    deployment script change

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,7 +5,7 @@ echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(php artisan down) || true
+#(php artisan down) || true
 
 # Pull the latest version of the app
 git pull origin production
@@ -26,6 +26,6 @@ npm run prod
 php artisan migrate --force
 
 # Exit maintenance mode
-php artisan up
+#php artisan up
 
 echo "Deployment finished!"
